c1232f303194fd8a51ed801f70892c6b83a91b98
Anbo Zhou <zhouanbo@anbodembp.lan>
2020-01-12 06:03:06 -0500

    added humanbase

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,17 @@
 const functions = require('firebase-functions');
+const fetch = require('node-fetch');
+const cors = require('cors')({origin: true});
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.humanbase = functions.https.onRequest((req, res) => {
+  cors(req, res, () => {});
+  fetch(`https://hb.flatironinstitute.org/api/genes/${req.query.id}/predictions/`).then((response) => {
+    return response.json();
+    })
+    .then((myJson) => {
+      res.send(myJson);
+    });
+  
+});
--- utils/update.sh
+++ utils/update.sh
@@ -23,7 +23,7 @@
 # gunzip GRCh38_latest_genomic.gff.gz
 # awk 'BEGIN{print("name\tstart\tend\tstrand\tdescription\tsynonym\tbiotype")}$0!~/^#/&&$3=="gene"{split($9,a1,"Name=");split(a1[2],a2,";");split($9,b1,"description=");split(b1[2],b2,";");split($9,c1,"gene_synonym=");split(c1[2],c2,";");split($9,d1,"gene_biotype=");split(d1[2],d2,";");print(a2[1]"\t"$1"\t"$4"\t"$5"\t"$7"\t"b2[1]"\t"c2[1]"\t"d2[1])}' FS="\t" OFS="\t" GRCh38_latest_genomic.gff | sed "s/\'//g" > GRCh38_latest.tsv
 # csvjson GRCh38_latest.tsv > GRCh38_latest.json
-node refseq-import.js
+# node refseq-import.js
 
 # DISEASE
 # wget http://download.jensenlab.org/human_disease_textmining_filtered.tsv
@@ -40,6 +40,13 @@ node refseq-import.js
 # csvjson human_disease_experiments_filtered.header.tsv > human_disease_experiments_filtered.json
 
 # node disease-import.js
+
+
+# ExAC
+# wget ftp://ftp.broadinstitute.org/pub/ExAC_release/release1/manuscript_data/forweb_cleaned_exac_r03_march16_z_data_pLI.txt.gz
+# gunzip forweb_cleaned_exac_r03_march16_z_data_pLI.txt.gz
+# csvjson forweb_cleaned_exac_r03_march16_z_data_pLI.txt > exac_r03.json
+# node exac-import.js
 
 # rm *tsv
 # rm *csv
